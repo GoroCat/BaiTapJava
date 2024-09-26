819b0de45bd140fdc3f7a7d42193f72f1d5a57b0
GoroCat <smolthjnk@gmail.com>
2024-09-26 22:00:29 +0700

    Update A_B.java

--- Lab2_giaiphuongtrinhbac1/src/A_B.java
+++ Lab2_giaiphuongtrinhbac1/src/A_B.java
@@ -1,8 +1,24 @@
-
+import java.util.Scanner;
 public class A_B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập hệ số a: ");
+    	double a = scanner.nextDouble();
+    	System.out.print("Nhập hệ số b: ");
+    	double b = scanner.nextDouble();
+    	if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình có vô số nghiệm");
+            } else {
+                System.out.println("Phương trình vô nghiệm");
+            }
+        } else
+        {
+            double x = -b / a;
+            System.out.println("Phương trình có nghiệm x = " + x);
+        }
 
 	}
 
