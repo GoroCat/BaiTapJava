a83e3802b6072e811e4a6bb53f1fe19b14af8cae
GoroCat <smolthjnk@gmail.com>
2024-09-27 13:16:59 +0700

    update

---
+++ Lab2_TinhSoDien/src/SD.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số điện sử dụng của tháng: ");
+        int soDien = scanner.nextInt();
+        double tienDien;
+        if (soDien <= 50) 
+        	{
+            tienDien = soDien * 1000;
+        	} else 
+        		{
+        		tienDien = 50 * 1000 + (soDien - 50) * 1200;
+        		}
+        System.out.println("Số tiền điện phải trả: " + tienDien + "VNĐ");
+    }
+}
