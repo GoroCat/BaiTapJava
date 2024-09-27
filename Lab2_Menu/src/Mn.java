a965e4b5123972b16ba53fb27a12de8ccb5f0275
GoroCat <smolthjnk@gmail.com>
2024-09-27 13:31:30 +0700

    done

--- Lab2_Menu/src/Mn.java
+++ Lab2_Menu/src/Mn.java
@@ -36,9 +36,70 @@ public class Mn {
         System.out.println("+-----------------------------------+");
     }
     public static void PTB1() {
+    	 Scanner scanner = new Scanner(System.in);
+         System.out.print("Nhập hệ số a: ");
+         double a = scanner.nextDouble();
+         System.out.print("Nhập hệ số b: ");
+         double b = scanner.nextDouble();
+         if (a == 0) {
+             if (b == 0) {
+                 System.out.println("Phương trình có vô số nghiệm");
+             } else {
+                 System.out.println("Phương trình vô nghiệm");
+             }
+         } else {
+             double x = -b / a;
+             System.out.println("Phương trình có nghiệmx = " + x);
+         }
     }
     public static void PTB2() {
+    	Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+        if (a == 0){
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình có vô số nghiệm");
+                } else {
+                    System.out.println("Phương trình vô nghiệm");}
+            } else {
+                double x = -c / b;
+                System.out.println("Phương trình có nghiệm x = " + x);}
+        	} else {
+            double delta = b * b - 4 * a * c;
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Phương trình có nghiệm kép x = " + x);
+            } else {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Phương trình có hai nghiệm phân biệt:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
     }
     public static void tinhTienDien() {
-    }
+    	Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số điện sử dụng trong tháng: ");
+        int soDien = scanner.nextInt();
+
+        double tienDien;
+
+        if (soDien <= 50) {
+            tienDien = soDien * 1000;
+        } else {
+            tienDien = 50 * 1000 + (soDien - 50) * 1200;
+        }
+
+        System.out.printf("Số tiền điện phải trả là: %.2f VND\n", tienDien);
+    } 
 }
